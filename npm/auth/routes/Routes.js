dda9da3d0947fc8dcc8c3372ba52967c94b4b643
shravan <shrvnbhat0@gmail.com>
2019-02-15 00:50:36 +0530

    Modifications to npm auth files

--- npm/auth/routes/Routes.js
+++ npm/auth/routes/Routes.js
@@ -11,13 +11,16 @@ module.exports = function(app){
 
     app.use('/Users',Users);
 
-    app.get('*', (req,res) => {
-        res.status(404);
-        res.sendFile(path.join(_dirname + '../views/404.html'));
-    });
+
     ///Stream page
     app.use('/Stream',Stream);
 
     ///Admin page
     app.use('/Admin',Admin);
+
+    //Error page
+    app.get('*', (req,res) => {
+        res.status(404);
+        res.sendFile(path.join(_dirname + '../views/404.html'));
+    });
 }
