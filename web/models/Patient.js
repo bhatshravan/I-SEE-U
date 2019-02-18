440b90144c90e721d738d1c8fc7dfec8b6dbbfb2
Shreyas Segu <shreyassegu@gmail.com>
2019-02-18 16:00:58 +0530

    Model Changes

--- web/models/Patient.js
+++ web/models/Patient.js
@@ -5,7 +5,15 @@ var PatientSchema = new mongoose.Schema({
     required: true
   },
   patientID: {
-      type: String,
-      required: true
-  }
+    type: String,
+    required: true
+  },
+  minutes: {
+    type: Number
+  },
+  relativesList: [{ name: String, phone: Number, email: String }]
 });
+
+var Patient = mongoose.model("Patient", PatientSchema);
+
+module.exports(Patient);
