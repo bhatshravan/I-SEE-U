205d202c2aa5205c0b3bf0b6cac98e8dcc4ef6f1
shravan <shrvnbhat0@gmail.com>
2019-02-18 10:58:09 +0530

    Major changes2

--- npm/auth/config.js
+++
@@ -1,7 +0,0 @@
-module.exports =  {
-    BUILD: 0,
-    mongodb_url : 'mongodb://127.0.0.1:27017/icu',
-    PORT: 8081,
-    secret: 'Double helix',
-    session_secret: "Double helix session"
-}
--- npm/auth/controllers/Stream_controller.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-exports.create = (req,res) => {
-    return res.status(400).json({ "Stream": "Stream error"});
-}
-exports.test = (req,res) => {
-    res.redirect('/login.html');
-}
