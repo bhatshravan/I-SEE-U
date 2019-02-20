6e34c70d2f27ac005569b20ee936691eac97d16c
shravan <shrvnbhat0@gmail.com>
2019-02-20 21:45:04 +0530

    Added stream bash files

--- stream/app.js
+++ stream/app.js
@@ -1,28 +1,31 @@
 const {NodeMediaServer} = require('node-media-server');
-
+const numCPUs = require('os').cpus().length;
 const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 60,
-    ping_timeout: 30
-  },
-  http: {
-    port: 8000,
-    mediaroot: './media',
-    allow_origin: '*'
-  },
-  trans: {
-    ffmpeg: '/usr/bin/ffmpeg',
-    tasks: [
-      {
-        app: 'live',
-        hls: true,
-        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]'
-      }
-    ]
-  }
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 60,
+        ping_timeout: 30
+    },
+    http: {
+        port: 8000,
+        mediaroot: './media',
+        allow_origin: '*'
+    },
+    trans: {
+        ffmpeg: '/usr/bin/ffmpeg',
+        tasks: [
+            {
+                app: 'live',
+                hls: true,
+                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]'
+            }
+        ]
+    },
+    cluster: {
+        num: numCPUs
+    }
 };
 
 var nms = new NodeMediaServer(config)
